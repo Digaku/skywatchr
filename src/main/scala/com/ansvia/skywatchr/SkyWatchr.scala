1a4a5b1a2426651708493c8f6d529b65ea76957e
robin <robin@digaku.com>
2012-12-03 23:06:46 +0700

    support OPTIONS for manually set groupid, default to group01

--- src/main/scala/com/ansvia/skywatchr/SkyWatchr.scala
+++ src/main/scala/com/ansvia/skywatchr/SkyWatchr.scala
@@ -5,16 +5,44 @@ import java.util.Properties
 import kafka.consumer.{KafkaListener, ConsumerConfig}
 
 object SkyWatchr {
+  
+    def getArg(args:Array[String],key:String):Option[String] = {
+      var i = 0
+      var rv:Option[String] = None
+      args.foreach { arg =>
+        if (arg.startsWith("--")){
+          if(arg.substring(2) == key){
+            val nextI = i + 1
+            if (args.length > nextI)
+              rv = Some(args(nextI))
+          }
+        }
+        i += 1
+      }
+      rv
+    }
+    
+    def getChannels(args:Array[String])={
+      val ss = args.reduce(_ + "|" + _)
+      val rv = """\-\-\w+\|\w+""".r.replaceAllIn(ss,"").split("\\|")
+      rv.filter(_.length > 0)
+    }
+  
     def main(args:Array[String]){
         
         if(args.length == 0){
-            println("Usage: java -jar skywatchr.jar [CHANNEL]")
+            println("Usage: java -jar skywatchr.jar [OPTIONS] [CHANNEL]")
+            println("")
+            println("OPTIONS are: ")
+            println("     --group    consumer group id.")
             return
         }
         
+        val group = getArg(args, "group").getOrElse("group01").toLowerCase
+        
         val props = new Properties()
         props.put("zk.connect", "localhost:2181")
-        props.put("groupid", "group01")
+        props.put("groupid", group)
         props.put("zk.sessiontimeout.ms", "400")
         props.put("zk.synctime.ms", "200")
         props.put("autocommit.interval.ms", "1000")
@@ -24,12 +52,13 @@ object SkyWatchr {
 
         Runtime.getRuntime.addShutdownHook(new Thread(){
           override def run(){
-            println("^C detected.")
+            println("Closed.")
             cons.shutdown()
           }
         })
 
-        println("Listening to: " + args.reduce(_ + "," + _))
+        val channels = getChannels(args)
+        println("[%s] listening for %s".format(group, channels.reduce(_ + "," + _)))
 
         cons.listen(args: _*){ d =>
           println(d.topic + " | " + d.message)
